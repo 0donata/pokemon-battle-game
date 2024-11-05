3cf99d1ac1fdf29177e6ca11180af4454c36af25
Maksym <adamiev94a@gmail.com>
2024-11-05 13:45:28 +0200

    server static files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const express = require("express");
+const path = require("path");
 
 dotenv.config();
 
@@ -19,3 +21,9 @@ mongoose
   .catch((error) => {
     console.error("Database connection error:", error);
   });
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+});
